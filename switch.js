79375afae1056a3b1dc72eb5ca9c5a011f9f27ba
rollen1 <nolovedivision@gmail.com>
2022-11-17 07:55:23 +0200

    some changes

---
+++ switch.js
@@ -0,0 +1,25 @@
+console.clear();
+
+const day = 3;
+
+switch (day) {
+    case 1:
+        console.log('pirmadienis');
+        break;
+    case 2:
+        console.log('antradienis');
+        break;
+    case 3:
+        console.log('treciadienis');
+        break;
+    case 4:
+        console.log('ketvirtadienis');
+        break;
+    case 5:
+        console.log('penktadienis');
+        break;
+    default: 
+        console.log('tokia diena neegzistuoja');
+        break;
+
+}
